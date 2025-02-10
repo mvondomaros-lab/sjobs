822ac59a10d59ccbc5ba28f9b11e2656a865c75c
Michael von Domaros <mvondomaros@uni-marburg.de>
2025-02-10 10:07:22 +0100

    Formatting.

--- src/sjobs/sjobs.py
+++ src/sjobs/sjobs.py
@@ -10,42 +10,21 @@ def main():
     """Show Slurm jobs."""
 
     # Define categories, fields, states, and colors.
+    common_fields = ["JobID", "Name", "NumCPUS", "tres-per-job"]
     categories = {
-        "Running": {
-            "fields": [
-                "JobID",
-                "Name",
-                "NumCPUs",
-                "cpus-per-tres",
-                "AllocNodes",
-                "EndTime",
-            ],
+        "RUNNING": {
+            "fields": common_fields + ["AllocNodes", "EndTime"],
             "states": ["R"],
             "color": "green",
         },
-        "Pending": {
-            "fields": [
-                "JobID",
-                "Name",
-                "NumCPUs",
-                "cpus-per-tres",
-                "Partition",
-                "SubmitTime",
-                "Reason",
-                "Dependency",
-            ],
+        "PENDING": {
+            "fields": common_fields
+            + ["Partition", "SubmitTime", "Reason", "Dependency"],
             "states": ["PD"],
             "color": "cyan",
         },
-        "Other": {
-            "fields": [
-                "JobID",
-                "Name",
-                "NumCPUs",
-                "cpus-per-tres",
-                "State",
-                "Reason",
-            ],
+        "OTHER": {
+            "fields": common_fields + ["Partition", "State", "Reason"],
             "states": [
                 "BF",
                 "CA",
@@ -76,22 +55,25 @@ def main():
 
     # Build the squeue command line.
     if shutil.which("squeue"):
-        cmdline = ["squeue", "--me"]
+        cmdline = []
     else:
-        cmdline = ["ssh", "marc3a", "squeue", "--me"]
+        cmdline = ["ssh", "marc3a"]
+    cmdline += ["squeue", "-me"]
 
     # Fetch job information.
     for cat, catdict in categories.items():
+        # TODO: Dirty fix for tres-per-job format.
+        headers = catdict["fields"].copy()
+        headers[3] = "TResPerJob"
         format_str = ":500,".join(catdict["fields"]) + ":500"
         state_str = ",".join(catdict["states"])
         color = catdict["color"]
 
         _cmdline = cmdline + [f"--Format={format_str}", f"--states={state_str}"]
         stdout = subprocess.run(_cmdline, capture_output=True).stdout.decode()
-        jobs = [line.split() for line in stdout.splitlines()]
-        if len(jobs) != 1:
-            click.echo(click.style(f"[{cat}]", fg=color, bold=True))
+        jobs = [line.split() for line in stdout.splitlines()[1:]]
+        if len(jobs) != 0:
+            click.echo(click.style(f"\n>>> {cat}", fg=color, bold=True))
             click.echo("\n")
-            jobs = tabulate.tabulate(jobs, headers="firstrow")
+            jobs = tabulate.tabulate(jobs, headers=headers, tablefmt="simple")
             click.echo(jobs)
-            click.echo("\n")
