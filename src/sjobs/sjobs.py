19e5ec0c0c6f8436f1ec2b8a5b10373862f656e6
Michael von Domaros <mvondomaros@uni-marburg.de>
2025-02-10 09:40:19 +0100

    Add support for categories.

--- src/sjobs/sjobs.py
+++ src/sjobs/sjobs.py
@@ -1,41 +1,99 @@
-import io
-import os
+import shutil
 import subprocess
 
 import click
-import pandas as pd
+import tabulate
 
 
 @click.command()
 def main():
     """Show Slurm jobs."""
 
-    # Define fields, their format codes, and their order.
-    fieldcodes = {
-        "Name": "%j",
-        "JobID": "%i",
-        "State": "%t",
-        "Reason": "%r",
-        "Nodes": "%N",
-        "TimeLeft": "%M",
+    # Define categories, fields, states, and colors.
+    categories = {
+        "Running": {
+            "fields": [
+                "JobID",
+                "Name",
+                "NumCPUs",
+                "cpus-per-tres",
+                "AllocNodes",
+                "EndTime",
+            ],
+            "states": ["R"],
+            "color": "green",
+        },
+        "Pending": {
+            "fields": [
+                "JobID",
+                "Name",
+                "NumCPUs",
+                "cpus-per-tres",
+                "Partition",
+                "SubmitTime",
+                "Reason",
+                "Dependency",
+            ],
+            "states": ["PD"],
+            "color": "cyan",
+        },
+        "Other": {
+            "fields": [
+                "JobID",
+                "Name",
+                "NumCPUs",
+                "cpus-per-tres",
+                "State",
+                "Reason",
+            ],
+            "states": [
+                "BF",
+                "CA",
+                "CD",
+                "CF",
+                "CG",
+                "DL",
+                "F",
+                "NF",
+                "OOM",
+                "PR",
+                "RD",
+                "RF",
+                "RH",
+                "RQ",
+                "RS",
+                "RV",
+                "SI",
+                "SE",
+                "SO",
+                "ST",
+                "S",
+                "TO",
+            ],
+            "color": "red",
+        },
     }
-    delimiter = "|"
 
-    # Fetch the squeue output.
-    stdout = subprocess.run(
-        [
-            "squeue",
-            "-u",
-            os.getlogin(),
-            f"--format='{delimiter.join(fieldcodes.values())}'",
-        ],
-        capture_output=True,
-    ).stdout.decode()
+    # Build the squeue command line.
+    if shutil.which("squeue"):
+        cmdline = ["squeue", "--me"]
+    else:
+        cmdline = ["ssh", "marc3a", "squeue", "--me"]
 
-    # Parse the squeue output.
-    df = pd.read_csv(
-        io.StringIO(stdout), sep=delimiter, skiprows=1, names=fieldcodes.keys()
-    ).sort_values(by="Name")
+    # Fetch job information.
+    for cat, catdict in categories.items():
+        format_str = ":500,".join(catdict["fields"]) + ":500"
+        state_str = ",".join(catdict["states"])
+        color = catdict["color"]
 
-    # Output
-    print(df.to_string(index=False))
+        stdout = subprocess.run(
+            cmdline + [f"--Format='{format_str}'", f"--states='{state_str}'"],
+            capture_output=True,
+        ).stdout.decode()
+        jobs = [line.split() for line in stdout.splitlines()]
+        if len(jobs) != 1:
+            click.echo(click.style(f"[{cat}]", fg=color, bold=True))
+            click.echo("\n")
+            jobs = tabulate.tabulate(jobs, headers="firstrow")
+            click.echo(jobs)
+            click.echo("\n")
